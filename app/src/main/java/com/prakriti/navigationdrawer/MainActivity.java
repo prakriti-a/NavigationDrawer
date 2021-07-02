b2b3fbb9b8f93a042823ffb82783db9ba50547f5
prakriti.a <prakriti.a@telxsi.com>
2021-07-02 13:21:03 +0530

    Initial commit

---
+++ app/src/main/java/com/prakriti/navigationdrawer/MainActivity.java
@@ -0,0 +1,20 @@
+package com.prakriti.navigationdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar myToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
